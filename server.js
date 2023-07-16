be05f786a6cf71033217ab9e5dc8c276f6bd3eb2
barathbalasubramanian <barathkumar.b2411@gmail.com>
2023-07-16 20:39:28 +0530

    find

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const axios = require('axios');
 const app = express();
 const bodyParser = require('body-parser');
 const add = require('./data/savedata')
+const connectToMongoDB = require('./mongodb/mongoconnection')
+const userSchema = require('./schemas/model')
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -34,16 +37,27 @@ const translateMessage = async (message, targetLanguage) => {
 };
 
 
-app.post('/', async (req, res) => {
+app.post('/' , async (req, res) => {
     const message = req.body.text;
     const targetLanguage = req.body.target
-    translated = await translateMessage(message, targetLanguage);
-    console.log(translated)
-    res.json({ trans : translated });
-
-    // save to db
-    add(message,translated)
-
+    
+    await connectToMongoDB()
+    try {
+      const results = await userSchema.find({text: message });
+      if (results.length) {
+        console.log(results[0].translatetext , '  Already there')
+        res.json({ trans : results[0].translatetext });
+      }
+      else {
+        console.log('yet to save')
+        // save to db
+        translated = await translateMessage(message, targetLanguage);
+        add(message,translated)
+        res.json({ trans : translated });
+      }
+    } catch (error) {
+      console.error(error);
+    }
 });
 
 
